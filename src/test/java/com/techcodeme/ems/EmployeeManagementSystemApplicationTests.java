9987b1bcee71ecfd3989d46b66d1670bd9794fe6
techcodeme00 <ss.maths23@gmail.com>
2026-08-04 22:39:44 +0530

    feat: initialize Spring Boot emplpoyee management project

---
+++ src/test/java/com/techcodeme/ems/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcodeme.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
